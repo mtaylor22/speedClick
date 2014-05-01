e04a9085f993849309ee3d6ab51416dcc335837a
Mitchell Taylor <mtaylor22@neo.tamu.edu>
2014-04-30 20:36:43 -0500

    Form Renaming (Supplement)
    "Team Explorer" missed a bit

--- speedClick/My Project/Application.Designer.vb
+++ speedClick/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.speedClick.Form1
+            Me.MainForm = Global.speedClick.ActionForm
         End Sub
     End Class
 End Namespace
